324f5e1a9c8a366999dbbc93a3ae95e892829d74
jonitsu <jonitsu@gmail.com>
2012-02-23 09:29:48 +0100

    Zrobione pobieranie pakietu, pobieranie pakietu dla konkretnego użytkownika oraz PackageManager, gdzie sprawdzane są jego możliwości według pobranych pakietów. Utworzone zapytanie dla ustawiania pakietu dla użytkownika.

---
+++ engine/system/class.packagemanager_Exception.php
@@ -0,0 +1,59 @@
+<?php
+
+class BrakAktywnychPakietow extends Exception {
+
+    public function __construct() {
+        parent::__construct('', 0);
+    }
+
+}
+
+class NieMoznaDodawacOfert extends Exception {
+
+    public function __construct() {
+        parent::__construct('', 0);
+    }
+
+}
+
+class NieMoznaDodawacUslug extends Exception {
+
+    public function __construct() {
+        parent::__construct('', 0);
+    }
+
+}
+
+class NieMoznaDodacLogo extends Exception {
+
+    public function __construct() {
+        parent::__construct('', 0);
+    }
+
+}
+
+class NieMoznaDodacWWW extends Exception {
+
+    public function __construct() {
+        parent::__construct('', 0);
+    }
+
+}
+
+class NieMoznaDodacBanera extends Exception {
+
+    public function __construct() {
+        parent::__construct('', 0);
+    }
+
+}
+
+class NieMoznaWlaczycMailingu extends Exception {
+
+    public function __construct() {
+        parent::__construct('', 0);
+    }
+
+}
+
+?>
