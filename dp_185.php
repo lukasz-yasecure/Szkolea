6c64c57b260c058e74cf688fcc16789f2e950d2e
Darijus Volosevic <webzio@zz.webizzo.com>
2012-03-22 13:24:49 +0100

    dotpay $_POST petla 	modified:   dp_185.php

--- dp_185.php
+++ dp_185.php
@@ -30,10 +30,13 @@ try
     $u = $um->getUserFromSession($sm);
     $dbc = new DBC($sys);
     
+    $post = '';
+    foreach($_POST as $k => $v) {
+        $post .= $k.'::'.$v.'&&';
+    }
     if($_SERVER['REMOTE_ADDR'] == '195.150.9.37') { // check IP
             
             $salt = '9ETs0gaZNS1ATAJx';
-            $post = 'Control: '.$_POST['control'].', opis: '.$_POST['description'].', email: '.$_POST['email'].', status: '.$_POST['t_status'].', id: '.$_POST['t_id'].', md5: '.$_POST['md5'];
             $md5 = md5($salt.':57265:'.$_POST['control'].':'.$_POST['t_id'].':'.$_POST['amount'].':'.$_POST['email'].':::::'.$_POST['t_status']);
             if($md5 == $_POST['md5']) { // check MD5
                 if($_POST['t_status'] == '2') { // wykonana
@@ -41,7 +44,7 @@ try
                     $dbc->query(Query::updateDotPay($_POST['control']));   
                     echo "OK";
                 } else { // 1 - nowa, 3 - odzrucona, 4, 5...
-                    $dbc->query(Query::logDotPay('2',$_POST['control']),$post);   
+                    $dbc->query(Query::logDotPay('2',$_POST['control'],$post));   
                     echo "OK";
                 }
             } else {
